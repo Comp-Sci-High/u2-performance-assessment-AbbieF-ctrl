eeaecd5ba9687a50c2a1959bac7140ee2deb175d
AbbieF-ctrl <abigail.foggarthy25@compscihigh.org>
2024-11-13 18:05:49 +0000

    I don't know

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const catImageRequestURL = "https://api.thecatapi.com/v1/images/search"
+const catFactsRequestURL = "https://cat-fact.herokuapp.com/facts"
 
+async function catImages() {
+    
+}
+
+async function catFacts() {
+
+}
+//I have no idea what to do next.
