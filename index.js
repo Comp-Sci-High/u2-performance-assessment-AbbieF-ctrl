ee6e6c136c700f3f5310c46014d0104eea0cb1ac
AbbieF-ctrl <abigail.foggarthy25@compscihigh.org>
2024-11-19 20:10:54 +0000

    I had to write my entire dame POST function, and it probably ain't even work

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-const catImageRequestURL = "https://api.thecatapi.com/v1/images/search"
-const catFactsRequestURL = "https://cat-fact.herokuapp.com/facts"
+const API_KEY= "live_Q11x3tiKEqQhTwHW7X0cVOebavvGmYn86Wk49qLsgbQKOiDuioimEfaCvHXmSiup"
 
-async function catImages() {
-    
+const catImageURL = "https://api.thecatapi.com/v1/images/search?api_key=" + API_KEY
+const catFactsURL = "https://cat-fact.herokuapp.com/facts"
+
+async function catImages(requestURL) {
+   const response = await fetch(requestURL)
+    const data = await response.json()
+	console.log(data)
+	return	data
 }
 
-async function catFacts() {
 
+async function catFacts(requestURL) {
+    const response = await fetch(requestURL)
+    const data = await response.json()
+    console.log(data)
+    return data 
 }
-//I have no idea what to do next.
+
+async function makeYourOwn() {
+    const options = {
+    method: "POST",
+    headers: {
+        Authorization: "Bearer live_Q11x3tiKEqQhTwHW7X0cVOebavvGmYn86Wk49qLsgbQKOiDuioimEfaCvHXmSiup",
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(catFactsURL),
+}
+const response = await fetch("https://cat-fact.herokuapp.com/facts", options);
+const data = await response.json();
+console.log(data)
+}
+
+makeYourOwn({
+    catFact: userInput,
+    catImage: userInput2
+})
+
+
+
+let userInput = prompt("Put in your own cat fact!")
+let userInput2 = prompt("also, put in a cat image.")
+
+//I'm not sure how to make a chatbot- so, take this instead
